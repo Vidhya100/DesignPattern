75f691db598223da0ddbbbed5e20027774b108c4
Vidhya Darade <vidhyadarade1@gmail.com>
2023-02-07 11:25:54 +0530

    Adapter and observer

---
+++ ObserverPattern/ConcreteSubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObserverPattern
+{
+        public class IBM : Stock
+        {
+            // Constructor
+            public IBM(string symbol, double price)
+                : base(symbol, price)
+            {
+            }
+        }
+}
